fbad8fea4ecf7c64e30bbce138cd3856e07b759c
Ajay Susanth <ajaysusanth10@gmail.com>
2024-05-12 00:13:10 +0530

    practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
+import Practice from "./Pages/Practice/Practice"
 
 function App() {
-  return <>hello</>;
+  return <><Practice/></>;
 }
 
 export default App;
---
+++ src/data.jsx
@@ -0,0 +1,39 @@
+import hlogo from "./assets/hackerrank logo.png"
+
+export const datas = [
+    {
+        "name":"HackerRank",
+        "logo": hlogo,
+        "link": "https://www.hackerrank.com/",
+    },
+
+    {
+        "name":"HackerRank",
+        "logo": hlogo,
+        "link": "https://www.hackerrank.com/",
+    },
+
+    {
+        "name":"HackerRank",
+        "logo": hlogo,
+        "link": "https://www.hackerrank.com/",
+    },
+
+    {
+        "name":"HackerRank",
+        "logo": hlogo,
+        "link": "https://www.hackerrank.com/",
+    },
+
+    {
+        "name":"HackerRank",
+        "logo": hlogo,
+        "link": "https://www.hackerrank.com/",
+    },
+
+    {
+        "name":"HackerRank",
+        "logo": hlogo,
+        "link": "https://www.hackerrank.com/",
+    },
+];
