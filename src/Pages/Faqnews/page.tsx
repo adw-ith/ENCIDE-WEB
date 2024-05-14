7ecf2731b26902b5724bbd5f8847af2c40917e89
Jithin-caz <117753804+Jithin-caz@users.noreply.github.com>
2024-05-14 22:19:49 +0530

    loader

--- src/Pages/Faqnews/page.tsx
+++ src/Pages/Faqnews/page.tsx
@@ -15,11 +15,11 @@ export default function Faqnews()
         <div className=' col-span-1 p-4 bg-slate-800'>
             <div className=' p-8 w-full'>
                 <h1 className=' text-center text-5xl text-slate-100  font-semibold'>Whats new in our world</h1>
-                <div className=" relative backdrop-blur-2xl text-white px-7 py-5 bg-slate-800 mt-5 rounded-xl shadow-2xl border border-slate-700">
-                    <div className=" absolute top-16 left-14 w-12 h-12 rounded-full bg-white blur-3xl"></div>
-                    <div className=" py-4 px-2 pt-0 sm:pt-4 sm:px-11"><img className=" w-full h-auto mt-6" src="https://www.teslarati.com/wp-content/uploads/2024/01/Tesla-Cybertruck-2024-production-builds-to-be-sold-out-soon-.jpg" alt="" /></div>
+                <div className=" relative group backdrop-blur-2xl text-white px-7 py-5 mt-5 rounded-xl shadow-2xl border border-slate-700">
+                    <div className=" -z-10 absolute group-hover:top-36 group-hover:left-36 ease-in-out duration-700 top-16 left-14 w-16 h-16 rounded-full bg-white blur-3xl"></div>
+                    <div className=" py-4 px-2 pt-0 sm:pt-4 sm:px-11"><img className=" w-full h-auto mt-6 rounded-lg shadow-sm group-hover:shadow-2xl" src="https://www.teslarati.com/wp-content/uploads/2024/01/Tesla-Cybertruck-2024-production-builds-to-be-sold-out-soon-.jpg" alt="" /></div>
                     <h1 className=" mt-3 text-2xl font-semibold">heading</h1>
-                    <p className=" mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus sapiente doloribus officiis rerum quasi distinctio expedita facilis ipsam asperiores libero accusantium, sequi pariatur fugiat officia ratione nam consequuntur qui placeat?</p>
+                    <p className=" mt-4 text-slate-400 group-hover:text-white ease-in-out duration-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus sapiente doloribus officiis rerum quasi distinctio expedita facilis ipsam asperiores libero accusantium, sequi pariatur fugiat officia ratione nam consequuntur qui placeat?</p>
                 </div>
             </div>
         </div>
