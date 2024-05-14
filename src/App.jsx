f067fc2a82f789364b173ef8c51fc564bbbdfc39
Jithin-caz <117753804+Jithin-caz@users.noreply.github.com>
2024-05-14 16:21:56 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Practice from "./Pages/Practice/Practice"
 function App() {
  const[load,setLoad]=useState(true)
   return <>
-  {load&& <div className=" fixed z-50 top-0 left-0 w-dvw h-dvh bg-slate-900 flex flex-col justify-center items-center">
+  {load&& <div style={{zIndex:'9999' }} className=" fixed z-50 top-0 left-0 w-dvw h-dvh bg-slate-900 flex flex-col justify-center items-center">
     <div className=" w-24 h-24 rounded-lg bg-slate-50 animate-spin"></div>
     <h1 className=" mt-6 text-white">loading in progress....</h1>
   </div>}
