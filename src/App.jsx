5c7b5db39f757a493b4cf8e4a248cf3313c33058
Jithin-caz <117753804+Jithin-caz@users.noreply.github.com>
2024-07-12 14:24:53 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 import AboutComponent from "./Pages/about/About.jsx";
+import EventComponent from "./Pages/events/events";
 import Faqnews from "./Pages/Faqnews/page";
 import Hero from "./Pages/hero/page";
 import Loader from "./Pages/loader/page";
 import NavComponent from "./Pages/navbar/navbar";
 import Practice from "./Pages/Practice/Practice"
+import Teamcarousel from "./Pages/Teams/teams";
 
 function App() {
  const[load,setLoad]=useState(true)
@@ -13,9 +15,9 @@ function App() {
   {load&& <Loader/>}
   <NavComponent/>
     <Hero loading={()=>setLoad(false)}/>
-  
   <AboutComponent/>
- 
+  <EventComponent/>
+  <Teamcarousel/>
   <Practice/>
   <Faqnews/></>;
 
